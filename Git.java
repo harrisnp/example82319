1b833b2f3e7d8177552b5cba76e3d43968d00b9a
Nathan Harris <harrisnp@s.dcsdk12.org>
2019-08-21 12:35:20 -0600

    Fixed up to date

--- Git.java
+++ Git.java
@@ -3,7 +3,7 @@ public class Git
     public static void main(String[] args)
     {
         System.out.println("\"git is Fun!!!!\"");
-        Sysem.out.println("Hi Mommy");
+        System.out.println("Hi Mommy");
         System.out.println("This is very hard");
         System.out.println("The time is 12:25 pm");
         System.out.println("The date is 8/21/19");
