043877eacbbbc557e19bf6156ce1c0d58209745e
Nathan Harris <harrisnp@s.dcsdk12.org>
2019-08-21 12:27:32 -0600

    State the date

--- Git.java
+++ Git.java
@@ -6,5 +6,6 @@ public class Git
         Sysem.out.println("Hi Mommy");
         System.out.println("This is very hard");
         System.out.println("The time is 12:25 pm");
+        System.out.println("The date is 8/21/19");
     }
 }
