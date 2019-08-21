ce9b3bd6109d6836e12e846d95285a7f2bc135e2
Nathan Harris <harrisnp@s.dcsdk12.org>
2019-08-21 12:26:38 -0600

    Say time, add missing semicolons

--- Git.java
+++ Git.java
@@ -3,7 +3,8 @@ public class Git
     public static void main(String[] args)
     {
         System.out.println("\"git is Fun!!!!\"");
-        Sysem.out.println("Hi Mommy")
-        System.out.println("This is very hard")
+        Sysem.out.println("Hi Mommy");
+        System.out.println("This is very hard");
+        System.out.println("The time is 12:25 pm");
     }
 }
